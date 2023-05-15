6b3145a1f9a4e091cd70ed1bc98f64c4100ac262
SherliArninda <118003920+SherliArninda@users.noreply.github.com>
2023-05-15 11:00:36 +0700

    Descending.c

--- Praktikum 5/Descending.c
+++ Praktikum 5/Descending.c
@@ -7,55 +7,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*Program Utama*/
-int main(void){
-    /* Kamus */
-    int A[15],B[15],C[100],i,j,n,k;
-    k=0; // nilai maksimum tabel A
+int main() {
+    //kamus lokal
+      int T[10] = {1,8,7,3,2,5,4,6,0,9};
+      int n = 10;
+      int i,j;
+      int temp;
 
-    /* Algoritma */
-    printf("=======COUNTING SORT DESCENDING=======\n");
-    printf("Masukan size array : ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        printf("Masukan data ke-%d : ",i);
-        scanf("%d",&A[i]);
-        if(A[i]>k){
-            k=A[i]; // mencari nilai maksimum
+    //algoritma
+      printf("Sebelum diurutkan : ");
+      for(i=0; i<n; i++) {
+        printf("%d ", T[i]);
+      }
+
+    //Pengurutan secara descending
+      for(i=0; i<n; i++) {
+        for(j=i+1; j<n; j++) {
+          if(T[i] < T[j]) {
+            int temp = T[i];
+            T[i] = T[j];
+            T[j] = temp;
+          }
         }
-    }
-    printf("Nilai maksimum array = %d\n",k);
-    printf("---------Sebelum---------\n");
-    for(i=1;i<=n;i++){ // print nilai array sebelum diurutkan
-        printf("%d ",A[i]);
-    }
-    // membuat array C dengan panjang k kemudian diinisialisasikan 0
-    for(i=0;i<=k;i++){
-        C[i] = 0;
-    }
+      }
+      printf("\n====================================");
+      printf("\nSetelah diurutkan secara descending: ");
+      for(i=0; i<n; i++) {
+        printf("%d ", T[i]);
+      }
 
-    // step 1 melakukan increment pada array C
-    for(j=1;j<=n;j++){
-        C[A[j]]++;
-    }
-
-    // step 2 menambahkan elemen berdasarkan elemen sebelumnya
-    for(i=k;i>=1;i--){
-        C[i-1] = C[i] + C[i-1];
-    }
-
-    // step 3 memasukan data ke array B dengan beberapa
-    for(j=1;j<=n;j++){
-        B[C[A[j]]] = A[j];
-        C[A[j]]--;
-    }
-
-    /* Output */
-    printf("\n---------Sesudah---------\n");
-    for(i=1;i<=n;i++){ // print nilai array setelah diurutkan
-        printf("%d ",B[i]);
-    }
-    printf("\n======================================\n");
     return 0;
 }
+
 
